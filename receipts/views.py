25eb578901f33e8c485dbdba2f49437fc4ea9e41
Daniel Lee <danieltlee18@gmail.com>
2023-01-19 12:07:37 -0500

    Feature 5 Complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Receipt, Account, ExpenseCategory
 
-# Create your views here.
+
+def receipt_list(request):
+    receipts = Receipt.objects.all()
+    context = {"receipts": receipts}
+    return render(request, "receipts/receipt_list.html", context)
